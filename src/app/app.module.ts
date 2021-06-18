6a579d260628a08b1f09781249be19e3bbca70b3
alexanderescobar <alexander.escobar@carvajal.com>
2021-06-18 18:50:50 -0500

    updates to deploy to heroku project 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent, ResumeComponent   ],
   imports: [
     BrowserModule,
     AppRoutingModule
